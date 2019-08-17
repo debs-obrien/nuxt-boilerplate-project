2fa90a29b7989d1371cf568f753a22ec9b526388
dobrien <dobrien@todopatterson.com>
2019-08-17 13:20:04 +0200

    nuxt starter

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
